429f0afd89259bac928800a59e8f8aea0dbb83a2
katysuu <97706886+katysuu@users.noreply.github.com>
2025-11-03 00:56:17 -0500

    hu01 (#6)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
 import { ReporteCentroMedico } from './components/reporte-centro-medico/reporte-centro-medico';
 import { ActualizarAlergias } from './components/actualizar-alergias/actualizar-alergias';
 
+
 export const routes: Routes = [
-  { path: '', redirectTo: 'reporte-centro-medico', pathMatch: 'full' },
+  { path: '', component: LoginComponent },
   { path: 'reporte-centro-medico', component: ReporteCentroMedico },
   { path: 'actualizar-alergias', component: ActualizarAlergias },
 ];
