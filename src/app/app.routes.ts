1ad45e841a03ef9c013bf88ca11f116bee7f2ffb
stephano-2002 <95435779+stephano-2002@users.noreply.github.com>
2025-11-02 21:03:10 -0500

    Stephano (#4)
    * hu09-hu12

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ReporteCentroMedico } from './components/reporte-centro-medico/reporte-centro-medico';
+import { ActualizarAlergias } from './components/actualizar-alergias/actualizar-alergias';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'reporte-centro-medico', pathMatch: 'full' },
+  { path: 'reporte-centro-medico', component: ReporteCentroMedico },
+  { path: 'actualizar-alergias', component: ActualizarAlergias },
+];
