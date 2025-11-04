25b3dbd49239d7d5cb22e6e7e20fe3a71a34b2c1
flzuazo <flzuazo@gmail.com>
2025-11-03 20:59:59 -0500

    HU02 y HU03

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ReporteCentroMedico } from './components/reporte-centro-medico/reporte-
 import { ActualizarAlergias } from './components/actualizar-alergias/actualizar-alergias';
 import { EmitirReceta } from './components/emitir-receta/emitir-receta';
 import { RegistroCentroMedico } from './components/registro-centro-medico/registro-centro-medico';
+import {RegistroPaciente} from './components/registro-paciente/registro-paciente';
+import {RegistroConsulta} from './components/registro-consulta/registro-consulta';
 
 
 
@@ -11,6 +13,8 @@ export const routes: Routes = [
   { path: '', component: Login },
   { path: 'reporte-centro-medico', component: ReporteCentroMedico },
   { path: 'actualizar-alergias', component: ActualizarAlergias },
-  { path: 'emitir-receta', component: EmitirReceta },
+  { path: 'emitir-receta', component: RegistroCentroMedico },
   { path: 'registro-centro-medico', component: RegistroCentroMedico },
+  { path: 'registro-paciente', component: RegistroPaciente },
+  { path: 'registro-consulta', component: RegistroConsulta },
 ];
