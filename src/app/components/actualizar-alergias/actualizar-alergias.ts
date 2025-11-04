1119d81420e032f37f630636179a88108c8e8923
stephano-2002 <95435779+stephano-2002@users.noreply.github.com>
2025-11-04 04:43:36 -0500

    Stephano (#18)
    * reporte centro medico
    
    * hu09-hu12
    
    * manteniento hu12
    
    * Fix table header in reporte-centro-medico.html
    
    * Refactor comments and clean up code formatting
    
    * hu12
    
    * mantenimiento hu12
    
    ---------
    
    Co-authored-by: katysuu <97706886+katysuu@users.noreply.github.com>

--- src/app/components/actualizar-alergias/actualizar-alergias.ts
+++ src/app/components/actualizar-alergias/actualizar-alergias.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-actualizar-alergias',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './actualizar-alergias.html',
   styleUrls: ['./actualizar-alergias.css']
 })
-export class ActualizarAlergias {
+export class ActualizarAlergias implements OnInit {
+
+  baseUrl = 'http://localhost:8080/api/alergias';
+  listaAlergias: any[] = [];
   alergia = {
+    id: null,
     idCentroMedico: '',
     idProfesional: '',
     fechaConsulta: '',
@@ -19,8 +24,58 @@ export class ActualizarAlergias {
     tratamiento: ''
   };
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.listar();
+  }
+
+  listar() {
+    this.http.get<any[]>(this.baseUrl).subscribe(data => {
+      this.listaAlergias = data;
+    });
+  }
+
   guardar() {
-    console.log('Datos guardados:', this.alergia);
-    alert('Información de alergias actualizada correctamente.');
+    if (this.alergia.id) {
+      // Actualizar
+      this.http.put(`${this.baseUrl}/${this.alergia.id}`, this.alergia).subscribe(() => {
+        alert('Alergia actualizada correctamente');
+        this.listar();
+        this.resetear();
+      });
+    } else {
+      // Guardar nueva
+      this.http.post(this.baseUrl, this.alergia).subscribe(() => {
+        alert('Alergia guardada correctamente');
+        this.listar();
+        this.resetear();
+      });
+    }
+  }
+
+  editar(a: any) {
+    this.alergia = { ...a };
+  }
+
+  eliminar(id: number) {
+    if (confirm('¿Desea eliminar este registro?')) {
+      this.http.delete(`${this.baseUrl}/${id}`).subscribe(() => {
+        alert('Alergia eliminada correctamente');
+        this.listar();
+      });
+    }
+  }
+
+  resetear() {
+    this.alergia = {
+      id: null,
+      idCentroMedico: '',
+      idProfesional: '',
+      fechaConsulta: '',
+      alergias: '',
+      diagnostico: '',
+      tratamiento: ''
+    };
   }
 }
