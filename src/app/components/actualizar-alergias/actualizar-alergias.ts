26cac1225e1484d4819388b0bd3df4df37226e33
katysuu <97706886+katysuu@users.noreply.github.com>
2025-11-04 04:44:48 -0500

    Revert "Stephano (#18)"
    This reverts commit 1119d81420e032f37f630636179a88108c8e8923.

--- src/app/components/actualizar-alergias/actualizar-alergias.ts
+++ src/app/components/actualizar-alergias/actualizar-alergias.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-actualizar-alergias',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './actualizar-alergias.html',
   styleUrls: ['./actualizar-alergias.css']
 })
-export class ActualizarAlergias implements OnInit {
-
-  baseUrl = 'http://localhost:8080/api/alergias';
-  listaAlergias: any[] = [];
+export class ActualizarAlergias {
   alergia = {
-    id: null,
     idCentroMedico: '',
     idProfesional: '',
     fechaConsulta: '',
@@ -24,58 +19,8 @@ export class ActualizarAlergias implements OnInit {
     tratamiento: ''
   };
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.listar();
-  }
-
-  listar() {
-    this.http.get<any[]>(this.baseUrl).subscribe(data => {
-      this.listaAlergias = data;
-    });
-  }
-
   guardar() {
-    if (this.alergia.id) {
-      // Actualizar
-      this.http.put(`${this.baseUrl}/${this.alergia.id}`, this.alergia).subscribe(() => {
-        alert('Alergia actualizada correctamente');
-        this.listar();
-        this.resetear();
-      });
-    } else {
-      // Guardar nueva
-      this.http.post(this.baseUrl, this.alergia).subscribe(() => {
-        alert('Alergia guardada correctamente');
-        this.listar();
-        this.resetear();
-      });
-    }
-  }
-
-  editar(a: any) {
-    this.alergia = { ...a };
-  }
-
-  eliminar(id: number) {
-    if (confirm('¿Desea eliminar este registro?')) {
-      this.http.delete(`${this.baseUrl}/${id}`).subscribe(() => {
-        alert('Alergia eliminada correctamente');
-        this.listar();
-      });
-    }
-  }
-
-  resetear() {
-    this.alergia = {
-      id: null,
-      idCentroMedico: '',
-      idProfesional: '',
-      fechaConsulta: '',
-      alergias: '',
-      diagnostico: '',
-      tratamiento: ''
-    };
+    console.log('Datos guardados:', this.alergia);
+    alert('Información de alergias actualizada correctamente.');
   }
 }
