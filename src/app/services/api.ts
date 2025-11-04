f73ab04899753f8ddb6e0d6b3c2e1298d3951080
Eduardo Flores <flzuazo@gmail.com>
2025-11-03 21:01:12 -0500

    HU02 Y 03 (#12)
    * Proyecto Angular inicial
    
    * Proyecto Angular inicial
    
    * HU02 y HU03
    
    * HU02 y HU03

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Paciente } from '../models/paciente';
+import { Consulta } from '../models/consulta';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Api {
-  
+export class ApiService {
+  private base = '/api'; // ajuste si tu base es otra
+
+  constructor(private http: HttpClient) {}
+
+  registrarPaciente(p: Paciente): Observable<Paciente> {
+    return this.http.post<Paciente>(`${this.base}/pacientes`, p);
+  }
+
+  registrarConsulta(c: Consulta): Observable<Consulta> {
+    return this.http.post<Consulta>(`${this.base}/consultas`, c);
+  }
+
 }
