a6a4805feaddf254785173653230c2b5140370ea
stephano-2002 <95435779+stephano-2002@users.noreply.github.com>
2025-11-06 21:33:32 -0500

    hu12-mantenimiento con conexion a backend (#25)
    * reporte centro medico
    
    * hu09-hu12
    
    * manteniento hu12
    
    * Fix table header in reporte-centro-medico.html
    
    * Refactor comments and clean up code formatting
    
    * hu12
    
    * mantenimiento hu12
    
    * hu12-mantenimiento con conexion a backend
    
    ---------
    
    Co-authored-by: katysuu <97706886+katysuu@users.noreply.github.com>

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Paciente } from '../model/paciente';
 import { Consulta } from '../model/consulta';
-import { Centromedico } from '../model/centromedico';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-
+import {PacienteHistorialDTO, PacienteHistorialResponse,PacienteHistorialItem} from '../model/paciente-historial';
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +15,35 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   registrarPaciente(p: Paciente): Observable<Paciente> {
-    return this.http.post<Paciente>(`${this.base}/nuevo_paciente`, p);
+    return this.http.post<Paciente>(`${this.base}/pacientes`, p);
   }
 
   registrarConsulta(c: Consulta): Observable<Consulta> {
     return this.http.post<Consulta>(`${this.base}/consultas`, c);
   }
-    registrarcentromedico(cm: Centromedico): Observable<Centromedico> {
-    return this.http.post<Centromedico>(`${this.base}/nuevo_centro_medico`, cm);
-  }
-}
 
+  // ===== HU12 =====
+  validarPacienteExiste(id: number): Observable<boolean> {
+    return this.http.get<boolean>(`${this.base}/exists/${id}`);
+  }
+
+  actualizarHistorialPaciente(
+    id: number,
+    body: PacienteHistorialDTO
+  ): Observable<PacienteHistorialResponse> {
+    return this.http.put<PacienteHistorialResponse>(
+      `${this.base}/paciente/historial/${id}`,
+      body
+    );
+  }
+  // NUEVOS: listar y eliminar registros del historial
+  listarHistorialPaciente(id: number) {
+    return this.http.get<PacienteHistorialItem[]>(`${this.base}/paciente/historial/registros/${id}`);
+  }
+
+  eliminarRegistroHistorial(registroId: number) {
+    return this.http.delete<void>(`${this.base}/paciente/historial/registros/${registroId}`);
+  }
+
+
+}
