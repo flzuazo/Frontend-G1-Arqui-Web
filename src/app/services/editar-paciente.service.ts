c00dc27133b7f93e535ae6e45e87793a828c575d
palomaaa18 <palomaromerosan@gmail.com>
2025-11-09 13:18:37 -0500

    Editar perfil paciente

---
+++ src/app/services/editar-paciente.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class EditarPacienteService {
+  private apiUrl = environment.apiURL;
+
+  constructor(private http: HttpClient) {}
+
+  obtenerPaciente(id: number) {
+    return this.http.get<any>(`${this.apiUrl}/pacientes/${id}`);
+  }
+
+  actualizarPaciente(id: number, data: any) {
+    return this.http.put(`${this.apiUrl}/actualizar_paciente/${id}`, data);
+  }
+}
