70a30a7834b3f1b17e4c3ba6d086debdb02494b8
flzuazo <flzuazo@gmail.com>
2025-11-22 00:01:07 -0500

    Solución de errores y testeo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -41,7 +41,6 @@ export const routes: Routes = [
     data: { roles: ['ADMIN', 'DOCTOR'] },
   },
   { path: 'editar-perfil', component: EditarPerfilPaciente },
-
   { path: '', component: Login },
   { path: 'menu', component: MenuPrincipal },
   { path: 'reporte-centro-medico', component: ReporteCentroMedico },
