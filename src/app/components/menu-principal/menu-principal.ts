30d98218a51378b2035e82c7319bf3ea7947da94
katysuu <katysue.1107@gmail.com>
2025-11-23 23:38:18 -0500

    Actualización de menú por rol

--- src/app/components/menu-principal/menu-principal.ts
+++ src/app/components/menu-principal/menu-principal.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-menu-principal',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule,],
   templateUrl: './menu-principal.html',
   styleUrls: ['./menu-principal.css'],
 })
-export class MenuPrincipal {}
+export class MenuPrincipal implements OnInit {
+
+rol: string | null = 'rol';
+
+ngOnInit(): void{
+  this.rol = localStorage.getItem('rol');
+  console.log('Rol en menú principal:', this.rol);
+}
+
+
+get esAdmin(): boolean {
+  return this.rol === 'ROLE_ADMIN';
+}
+
+get esProfesional(): boolean {
+  return this.rol === 'ROLE_PROFESIONALSALUD';
+}
+
+get esPaciente(): boolean {
+  return this.rol === 'ROLE_PACIENTE';
+}
+
+}
