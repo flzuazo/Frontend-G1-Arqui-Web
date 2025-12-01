7c96690e8618639cf75f14721b156e95dcfb9c39
katysuu <katysue.1107@gmail.com>
2025-11-30 22:39:18 -0500

    2 graficos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal, inject } from '@angular/core';
-import {Router, NavigationEnd, RouterOutlet} from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Navbar } from './components/navbar/navbar';
 import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs/operators';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,14 @@ import { filter } from 'rxjs/operators';
     Navbar,
     Footer,
     RouterOutlet,
+    HttpClientModule
   ],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('Frontend-G1-Arqui-Web');
-
   showNavbar = true;
-
   private router = inject(Router);
 
   constructor() {
@@ -29,7 +29,6 @@ export class App {
       .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe((event) => {
         const hideNavbarRoutes = ['/', '/login'];
-
         this.showNavbar = !hideNavbarRoutes.includes(event.urlAfterRedirects);
       });
   }
